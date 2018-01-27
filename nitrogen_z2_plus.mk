3fecd6967be485474284e17756f2db4122de5be5
Valeriano <jneto3000@gmail.com>
2018-01-26 21:14:46 -0300

    z2_plus: Move APNs config to vendor

--- nitrogen_z2_plus.mk
+++ nitrogen_z2_plus.mk
@@ -45,9 +45,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Missing Dependencies
 ALLOW_MISSING_DEPENDENCIES := true
 
-# Apn from marlin
-PRODUCT_COPY_FILES := device/zuk/z2_plus/configs/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
